7f0d490509e24bfec78283fdc9f375ca52c91f45
Dexter <denus.denisovuch@gmail.com>
2020-09-07 12:54:02 +0300

    Added some pages

--- app/pages/views.py
+++ app/pages/views.py
@@ -7,3 +7,15 @@ def index(request):
 
 def about(request):
     return render(request, 'pages/about.html')
+
+def menu(request):
+    return render(request, 'pages/menu.html')
+
+def blog(request):
+    return render(request, 'pages/blog.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def elements(request):
+    return render(request, 'pages/elements.html')
