9cbb9097c082ecdabb3c07743179575f9eab0ea9
Dexter <denus.denisovuch@gmail.com>
2020-09-08 14:16:22 +0300

    Added admin panel

--- app/pages/admin.py
+++ app/pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Food
 
-# Register your models here.
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description', 'food_type', 'image')
+    list_display_links = ('name', 'price', 'description', 'food_type')
+
+
+admin.site.register(Food, FoodAdmin)
--- app/pages/models.py
+++ app/pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Food(models.Model):
+    name = models.CharField("Назва страви", max_length=50)
+    price = models.IntegerField("Ціна страви")
+    description = models.TextField("Опис страви", max_length=250)
+    image = models.ImageField(upload_to='food/%Y/%m/%d/')
+    food_type = models.CharField("Тип страви",  max_length=50)
+
+    def __str__(self):
+        return self.name
